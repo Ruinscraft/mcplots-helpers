ab704e0623cd6572bceb5eff4b8c662e424e1acb
Dexton Anderson <contact@dextonanderson.com>
2020-09-03 11:40:16 -0500

    Fix monthly plot give

--- src/main/java/net/mcplots/helpers/MonthlyPlotGiveHistory.java
+++ src/main/java/net/mcplots/helpers/MonthlyPlotGiveHistory.java
@@ -1,6 +1,8 @@
 package net.mcplots.helpers;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -13,19 +15,23 @@ public abstract class MonthlyPlotGiveHistory {
 
     // Note: of current year
     public CompletableFuture<Boolean> hasRecievedPlotForMonth(UUID mojangId, int month) {
-        return getMonthlyPlotGiveHistory(mojangId).thenApply(result -> {
-            LocalDate now = LocalDate.now();
+        final LocalDate now = LocalDate.now();
 
-            for (long time : result) {
-                LocalDate dateOfPlotRecv = LocalDate.ofEpochDay(time);
+        return getMonthlyPlotGiveHistory(mojangId).thenApplyAsync(result -> {
+            try {
+                for (long time : result) {
+                    LocalDate dateOfPlotRecv = Instant.ofEpochMilli(time).atZone(ZoneId.systemDefault()).toLocalDate();
 
-                // if current year
-                if (dateOfPlotRecv.getYear() == now.getYear()) {
-                    // if requested month
-                    if (dateOfPlotRecv.getMonth().getValue() == month) {
-                        return true;
+                    // if current year
+                    if (dateOfPlotRecv.getYear() == now.getYear()) {
+                        // if requested month
+                        if (dateOfPlotRecv.getMonth().getValue() == month) {
+                            return true;
+                        }
                     }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
             return false;
