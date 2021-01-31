a5502c1244a4ad6d49d553a4b157ca1e3834c20c
Dexton Anderson <contact@dextonanderson.com>
2021-01-31 00:27:49 -0600

    Disable explosions

--- src/main/java/net/mcplots/helpers/WorldListener.java
+++ src/main/java/net/mcplots/helpers/WorldListener.java
@@ -3,8 +3,10 @@ package net.mcplots.helpers;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Fireball;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 
@@ -17,6 +19,11 @@ public class WorldListener implements Listener {
                     || entity.getType() == EntityType.FIREBALL) {
                 entity.remove();
             }
+
+            if (entity instanceof TNTPrimed
+                    || entity.getType() == EntityType.PRIMED_TNT) {
+                entity.remove();
+            }
         }
     }
 
@@ -26,6 +33,16 @@ public class WorldListener implements Listener {
                 || event.getEntityType() == EntityType.FIREBALL) {
             event.setCancelled(true);
         }
+
+        if (event.getEntity() instanceof TNTPrimed
+                || event.getEntity().getType() == EntityType.PRIMED_TNT) {
+            event.getEntity().remove();
+        }
+    }
+
+    @EventHandler
+    public void onExplode(EntityExplodeEvent event) {
+        event.setCancelled(true);
     }
 
 }
